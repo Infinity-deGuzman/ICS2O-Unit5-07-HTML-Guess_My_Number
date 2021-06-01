7ade1b21b3443c9b69f207fabb4453791ac0ef87
InfinitydeGuzma <>
2021-06-01 03:34:31 +0000

    removed while loop

--- js/script.js
+++ js/script.js
@@ -13,13 +13,12 @@ function doMathClicked () {
 	const actualNumber = Math.floor(Math.random() * 9)  // returns a random integer from 0 to 9
 	var counter = 0
 
-  while (userGuess !== actualNumber) {
-		if (userGuess < actualNumber) {
+
+	if (userGuess < actualNumber) {
 			document.getElementById('answer').innerHTML = userGuess + "is too high"
-		} else if (userGuess < actualNumber) {
+	} else if (userGuess < actualNumber) {
 			document.getElementById('answer').innerHTML = userGuess + "is too low"
-		}
-	} if (userGuess == actualNumber) {
+	} else {
 		document.getElementById('answer').innerHTML = "Congrats! You got it right! The hidden number is " + answer
 	}
 }
